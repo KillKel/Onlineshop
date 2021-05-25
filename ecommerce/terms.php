4ad221e1e08b7e02444aae8e767742511378f0d0
Kelyn Mae Rapal <kelyn.mae.rapal@gmail.com>
2021-05-25 21:30:46 +0800

    Version 1 by Kelyn Mae Rapal

---
+++ ecommerce/terms.php
@@ -0,0 +1,146 @@
+   <!--Modal Log-out --> 
+
+            <div class="modal fade" id="logout" tabindex="-1">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                </div>
+                <div class="modal-body">
+<h1>Official Shop Terms of Service</h1>
+ <p>
+Official Shops are participating Sellers that have been carefully selected by Shopee (such Sellers, “Official Shops”) from whom Shopee-registered Buyers will be able to enjoy the rights and privileges described in these Official Shop Terms of Services (“Official Shop Terms of Service”) when purchasing Items from Official Shops (“Official Shop Items”). If you are a Buyer, you will be deemed to have consented to the terms and conditions described in these Official Shop Terms of Service when you make purchases on Shopee. Similarly, if you are an Official Shop, you will be deemed to have consented to the terms and conditions described in these Official Shop Terms of Service by your continued participation in Shopee as an Official Shop. These Official Shop Terms of Service supplement the Terms of Service and the other Shopee Policies, of which these Official Shop Official Shop Terms of Service forms a part – please read the Terms of Service and the other Shopee Policies available on the Site as they contain important information regarding your rights and obligations.  Any terms that are used in these Official Shop Terms of Service that are not defined here will have the meanings given to such terms in the Terms of Service and/or the applicable Shopee Policy.  All terms and conditions described in these Official Shop Terms of Service are subject to change at Shopee’s discretion pursuant to the Terms of Service.<br><br><br>
+
+ 
+
+1.        Official Shop Cancellation, Return and Refund Policy<br><br>
+
+ 
+
+1.1     Buyer may only cancel his/her order prior to the payment of Buyer’s Purchase Monies into Shopee Guarantee Account.
+
+ 
+
+<br><br>1.2    Buyer may apply for the return of the purchased item and refund prior to the expiry of Shopee Guarantee Period, if applicable, subject to and in accordance with Shopee’s Refunds and Return Policy. Please refer to Shopee’s Refunds and Return Policy for further information.
+
+ 
+
+<br><br>1.3     Shopee reserves the right to cancel any transaction on the Site and Buyer agrees that Buyer’s sole remedy will be to receive a refund of the Buyer’s Purchase Monies paid into Shopee Guarantee Account.
+
+ 
+
+<br><br>1.4    If you have redeemed Shopee Coin for your transaction and you are successful in obtaining a refund based on Shopee’s Refunds and Return Policy, Shopee shall refund the monies you have actually paid for the item and credit back any redeemed Shopee Coin to your Account separately. 
+
+ 
+
+<br><br>2.       Official Shop Terms and Conditions
+
+ 
+
+<br><br>2.1     If you are an Official Shop, you should have received a separate written notification from Shopee informing you of your selection to participate as an Official Shop.  If you decide not to participate as an Official Shop at any time, please inform Shopee in writing; otherwise, you will be deemed to have elected to continue your participation as an Official Shop and consented to the terms and conditions set out in these Official Shop Terms of Service.   
+
+ 
+
+<br><br>2.2     Each Official Shop warrants that the Official Shop Item it offers on Shopee and actually delivered to Buyer (a) complies with (i) applicable law and (ii) the specifications listed (and made available to Buyer) on the Site, including any and all product warranties, specifications, drawings, samples, performance criteria and all applicable quality, safety and hygiene requirements, and (b) will be free from defects.  Each Official Shop understands and agrees that it will be responsible for any non-conformity or defect in, or any public or private recall of, any of the Official Shop Items, and will hold Shopee harmless from any losses relating to the foregoing.
+
+ 
+
+<br><br>2.3     If an Official Shop is found to have offered or sold any Official Shop Item on Shopee that is restricted under Shopee’s Prohibited and Restricted Items Policy,  Shopee reserves the right to, at its sole discretion, remove the offending Official Shop Items from Shopee, and execute refunds (and any adjustments) to Buyers who have purchased such Official Shop Items (even if Buyers did not comply with any of the requirements described in Section 1 above), in which case Official Shop agrees to reimburse Shopee for all such refunds (including any related costs and charges) and authorizes Shopee to deduct such amounts from any amounts payable to Official Shop or take such other action as deemed appropriate by Shopee in its discretion.
+
+ 
+
+<br><br>2.4    Shopee’s determination to approve a refund or return of an Official Shop Item pursuant to Section 1 above is binding on the relevant Official Shop.  Official Shops agree to comply and do all such things as necessary to give effect to a Buyer’s request for a refund or return approved by Shopee.
+
+ 
+
+<br><br>2.5    Shopee will provide Return Air Waybills to Official Shops at no additional cost to Official Shops, and each Official Shop will be responsible for providing the relevant Return Air Waybill to Buyers when fulfilling the purchase order made by Buyers on Shopee.  If an Official Shop has failed to provide the proper Return Air Waybill to a Buyer whose request for refund or return has been approved by Shopee, Shopee reserves the right to elect any method of logistics to facilitate the return of the relevant Official Shop Items from such Buyer.  Official Shops understand and agree that in the case of the foregoing, Shopee has the right to recover from the relevant Official Shop any costs incurred in returning the Official Shop Item(s) without a Return Air Waybill, including by deducting such amounts from any amounts payable to such Official Shop.
+
+ 
+
+<br><br>2.6   For any refund or return request approved by Shopee, Shopee will organize the delivery of the relevant returned Official Shop Item to the address provided by the relevant Official Shop to Shopee in writing for the completion of the refund and return process, at no additional cost to such Official Shop so long as such address is in the country in which the relevant Official Shop Item was listed for sale on the Site (a “Local Address”).  If Official Shop fails to provide a Local Address for return of the returned Official Shop Item or otherwise fails to accept delivery of the returned Official Shop Item within a reasonable period of time (as determined by Shopee), Shopee reserves the right to dispose of such Official Shop Item in any manner it sees fit and Official Shop shall be deemed to have forfeited all rights to such Official Shop Item.
+
+ 
+
+<br><br>2.7    For any refund or return rejected by Shopee where the relevant Official Shop Item was received by Shopee, Shopee will organize the delivery of such Official Shop Item to the relevant Buyer’s address pursuant to Section 1.6 above.    
+
+ 
+
+<br><br>3.      Shopee 100% Authentic Product Policy
+
+ 
+
+<br><br>3.1    All products listed by Official Shops must be 100% authentic. 
+
+ 
+
+<br><br>3.2    Counterfeit products are prohibited from being listed in Shopee.  The term “counterfeit” encompasses a product that was made in exact imitation of an existing brand with the intention to deceive or defraud and may include, but is not limited to:
+
+ 
+
+<br><br>·    A product that violates any local country laws in which they are sold;
+
+ 
+
+<br><br>·    A product that is a fake or a replica of an existing official product;
+
+ 
+
+<br><br>·    A product that has never been produced by a specific brand;
+
+ 
+
+<br><br>·    A product that is significantly under-priced;
+
+ 
+
+<br><br>·    A product containing a registered trademark on the product, the packaging or anywhere related to the product without the authorization of the trademark owner;
+
+ 
+
+<br><br>·    A product that contains specific design elements (e.g. cartoon characters or colourways) protected by a trademark; without the authorization of the trademark owner;
+
+ 
+
+<br><br>·    A product that bears such similarities with other products (e.g. a replica of a branded item with or without altered logos) without the authorization of the trademark owner;
+
+ 
+
+<br><br>·    A product that has no visible trademark on the product image on the listing, but the delivered product includes a trademark; and
+
+ 
+
+<br><br>·    Usage of a photo on the listing that contains the trademark of a brand.
+
+ 
+
+<br><br>3.3    Original equipment manufacturer (“OEM”) brands are permitted as long as no official brand logo is displayed on any OEM product image and no official brand name is stated in any part of the product information.
+
+ 
+
+<br><br>3.4   If requested by Shopee, the Official Shop shall promptly provide documentation to Shopee that proves (to Shopee’s reasonable satisfaction) the authenticity of the Official Shop Item including, but not limited to, manufacturer sales invoices, authorized distribution agreements and/or certificates of authenticity (“Proof of Authenticity”).
+
+ 
+
+<br><br>3.5     If the Official Shop fails to provide Proof of Authenticity, the Official Shop shall:
+
+ 
+
+<br><br>(a)    accept the return of the Official Shop Item from the Buyer;
+
+ 
+
+<br><br>(b)    refund to the Buyer twice the amount that the Buyer paid for the Official Shop Item or, where Shopee has already refunded this amount to the Buyer, the Official Shop shall refund such amount to Shopee;
+
+ 
+
+<br><br>(c)    be liable to compensate Shopee for such other costs, expenses, losses and liabilities suffered or incurred by Shopee (including, but not limited to, loss of goodwill) as determined at Shopee’s sole discretion.
+
+ 
+
+<br><br>3.6   In the event that Shopee determines, at its sole discretion, that Official Shop has violated Section 3, Shopee may take all appropriate measures, including but not limited to removing the counterfeit Official Shop Item listing from the Site, prohibiting the publication of the same or similar goods and/or restricting, suspending or terminating the Official Shop's Shopee account and/or access to Shopee.  Official Shop's obligations and liabilities under these Terms of Service will not be affected by the restriction, suspension or termination of the Official Shop’s account.</p>
+
+</div>
+ 
+</div>
+</div>    
+</div>  
+                                      
